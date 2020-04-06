e206d34ed47c74358f8618ebd4ca9bb2476f0c54
BrazhnikovV <brazhnikov_ve@mail.ru>
2020-04-06 21:34:20 +0300

    Репозиторий для сущности привилегии и постраничная разбивка на странице списка привилегий.

--- module/User/src/Entity/Permission.php
+++ module/User/src/Entity/Permission.php
@@ -6,7 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * This class represents a permission.
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="\User\Repository\PermissionRepository")
  * @ORM\Table(name="permission")
  */
 class Permission
@@ -18,21 +18,21 @@ class Permission
      */
     protected $id;
 
-    /** 
-     * @ORM\Column(name="name")  
+    /**
+     * @ORM\Column(name="name")
      */
     protected $name;
-    
-    /** 
-     * @ORM\Column(name="description")  
+
+    /**
+     * @ORM\Column(name="description")
      */
     protected $description;
 
-    /** 
-     * @ORM\Column(name="date_created")  
+    /**
+     * @ORM\Column(name="date_created")
      */
     protected $dateCreated;
-    
+
     /**
      * @ORM\ManyToMany(targetEntity="User\Entity\Role", mappedBy="permissions")
      * @ORM\JoinTable(name="role_permission",
@@ -45,17 +45,17 @@ class Permission
     /**
      * Constructor.
      */
-    public function __construct() 
+    public function __construct()
     {
         $this->roles = new ArrayCollection();
     }
-    
-    public function getId() 
+
+    public function getId()
     {
         return $this->id;
     }
 
-    public function setId($id) 
+    public function setId($id)
     {
         $this->id = $id;
     }
@@ -64,32 +64,32 @@ class Permission
     {
         return $this->name;
     }
-    
+
     public function setName($name)
     {
         $this->name = $name;
     }
-    
+
     public function getDescription()
     {
         return $this->description;
     }
-    
+
     public function setDescription($description)
     {
         $this->description = $description;
     }
-    
+
     public function getDateCreated()
     {
         return $this->dateCreated;
     }
-    
+
     public function setDateCreated($dateCreated)
     {
         $this->dateCreated = $dateCreated;
     }
-    
+
     public function getRoles()
     {
         return $this->roles;
